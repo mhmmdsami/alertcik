5e2b8f7bdc0ca695d807db80106591a7895ffa20
oguzhanoztrkr <oguzhanoztrkr@gmail.com>
2018-09-16 16:06:56 +0300

    güncellemeler

--- js/alercik.js
+++ js/alercik.js
@@ -1,45 +1,52 @@
 class Alertcik {
-
-    constructor(options) {
+    alert(options) {
         document.querySelectorAll("*").forEach((element) => {
             this.zIndex = element.style.zIndex > this.zIndex ? element.style.zIndex + 1 : ((this.zIndex !== undefined) ? this.zIndex : 1);
         });
 
         this.options = {
-            width : 150,
-            height : 150,
             title : 'Alertcik',
             message : 'Welcome Alertcik!',
             position : 'center',
-            onClosed : function() {
-                alert("11");
-            }
+            btntext : 'Ok',
+            onClosed : undefined,
+            onOk : undefined
         };
 
         if(options !== undefined) {
-            options.width   !== undefined ? this.options.width   = options.width : undefined;
-            options.height  !== undefined ? this.options.height  = options.height : undefined;
-            options.title   !== undefined ? this.options.title   = options.title : undefined;
+            options.title   !== undefined ? this.options.title   = options.title   : undefined;
             options.message !== undefined ? this.options.message = options.message : undefined;
+            options.onOk !== undefined ? this.options.onOk = options.onOk : undefined;
+            options.onClosed !== undefined ? this.options.onClosed = options.onClosed : undefined;
+            options.position !== undefined ? this.options.position = options.position : undefined;
+            options.btntext !== undefined ? this.options.btntext = options.btntext : undefined;
         }
 
-        this.template = '<div class="alertcik alertcik-'+this.options.position+'" style="width: '+this.options.width+'px; height: '+this.options.height+'px;z-index: '+this.zIndex+'">' +
-                            '<div class="alertcik-header">'+
-                                '<div style="float:left;width:90%;">'+ this.options.title + '</div>'+
-                                '<div class="alertcik-close" style="float:left;width:9%;"><i class="fa fa-window-close"></i></div>'+
-                            '</div>'+
-                            '<div class="alertcik-body">'+this.options.message+'</div>'+
+        console.log(this.options);
+
+        this.template = '<div class="alertcik '+this.options.position+'" style="z-index:'+this.zIndex+'">'+
+                            '<div class="alertcik-header">'
+                                + this.options.title +
+                            '</div>' +
+                            '<div class="alertcik-close">' +
+                                '<i class="material-icons" style="font-size:18px;">close</i>' +
+                            '</div>' +
+                            '<hr>' +
+                            '<div class="alertcik-body">'
+                                + this.options.message +
+                            '</div>' +
+                            '<hr>' +
+                            '<div class="alertcik-buttons">' +
+                                '<span class="alertcik-btn">'
+                                    + this.options.btntext +
+                                '</span>' +
+                            '</div>' +
                         '</div>';
-    }
 
-    alert() {
-        this.appendChild();
-    }
-
-    appendChild() {
         let el = new DOMParser().parseFromString(this.template , 'text/html').body.firstChild;
         let e  = document.body.appendChild(el);
         let onClosedFnc = this.options.onClosed;
+        let onOkFnc = this.options.onOk;
         e.getElementsByClassName("alertcik-close")[0].onclick = function() {
             this.closest(".alertcik").remove();
             if(onClosedFnc !== undefined)
@@ -47,5 +54,15 @@ class Alertcik {
                 onClosedFnc();
             }
         }
+        
+        e.getElementsByClassName("alertcik-btn")[0].onclick = function() {
+            if(onOkFnc !== undefined)
+            {
+                onOkFnc();
+            }
+            this.closest(".alertcik").remove();
+        }
     }
 }
+
+var alertcik = new Alertcik();
